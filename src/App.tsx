65f63227f0500c47625893597dc6a992b1b5bebf
JulioLafise <jcperez@lafise.com>
2023-06-09 14:06:46 -0600

    Config sokcket client io

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BandAdd } from './components/BandAdd';
 import { BandList } from './components/BandList';
+import * as io from 'socket.io-client';
+
+const connectSocketServer = () => {
+  const socket = io.connect('http://localhost:8080', { transports: ['websocket'] });
+  return socket;
+}
 
 const App = () => {
+  const [socket] = useState(connectSocketServer());
+  const [online, setOnline] = useState(false);
+
+  useEffect(() => {
+    console.log(socket, 'Estado del socket');
+    setOnline(socket.connected);
+  }, [socket]);
+
+  useEffect(()=> {
+    socket.on('connect', () => {
+      setOnline(true);
+    });
+  }, [socket]);
+
+  useEffect(()=> {
+    socket.on('disconnect', () => {
+      setOnline(false);
+    });
+  }, [socket]);
+
   return (
     <div className="container px-52 py-10">
       <div className="px-10 py-10">
         <p className="font-bold">
           Service status:
-          <span className="text-green-500"> Online</span>
-          <span className="text-red-500"> Offline</span>
+          {
+            online
+              ? (<span className="text-green-500"> Online</span>)
+              : (<span className="text-red-500"> Offline</span>)
+          }
         </p>
       </div>
 
